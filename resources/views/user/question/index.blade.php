2610033a95c1b0f2cd6e769d05a9386e86bd0fb5
YUICHIRO WADA <wada1257@gmail.com>
2019-10-08 10:29:22 +0900

    カテゴリーボタンのクリックの際のクラス付与の修正

--- resources/views/user/question/index.blade.php
+++ resources/views/user/question/index.blade.php
@@ -39,7 +39,7 @@
           <td class="col-xs-1"><img src="{{ $question->user->avatar }}" class="avatar-img"></td>
           <td class="col-xs-2">{{ $question->tagCategory->name }}</td>
           <td class="col-xs-6">{{ mb_strimwidth($question->title, 0, 30, '...', 'UTF-8') }}</td>
-          <td class="col-xs-1">{{ $question->comment->count()}}<span class="point-color"></span></td>
+          <td class="col-xs-1">{{ $question->comments->count()}}<span class="point-color"></span></td>
           <td class="col-xs-2">
             <a class="btn btn-success" href="{{ route('question.show', $question->id) }}">
               <i class="fa fa-comments-o" aria-hidden="true"></i>
