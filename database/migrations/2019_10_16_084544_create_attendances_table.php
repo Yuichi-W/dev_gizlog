153e626e12f0671c2dd6fec9cc5f12ebec9dfd71
YUICHIRO WADA <wada1257@gmail.com>
2019-10-17 14:43:29 +0900

    マイグレーション＆シーディング

---
+++ database/migrations/2019_10_16_084544_create_attendances_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAttendancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attendances', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->date('date_time');
+            $table->dateTime('start_time')->nullable();
+            $table->dateTime('end_time')->nullable();
+            $table->integer('absent_status')->default(0);
+            $table->string('absent_reason', 500)->nullable();
+            $table->integer('revision_status')->default(0);
+            $table->string('revision_request', 500)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attendances');
+    }
+}
---
+++ database/seeds/AttendancesTableSeeder.php
@@ -0,0 +1,90 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+
+class AttendancesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('attendances')->truncate();
+        DB::table('attendances')->insert([
+
+            [
+                'user_id' => 1,
+                'date_time' => Carbon::create('2019', '10', '01'),
+                'start_time' => Carbon::create('2019', '10', '01', '09', '45', '00'),
+                'end_time' => Carbon::create('2019', '10', '01', '19', '05', '00'),
+                'absent_status' => 0,
+                'absent_reason' => null,
+                'revision_status' => 0,
+                'revision_request' => null,
+            ],
+            [
+                'user_id' => 2,
+                'date_time' => Carbon::create('2019', '10', '02'),
+                'start_time' => Carbon::create('2019', '10', '02', '10', '15', '20'),
+                'end_time' => Carbon::create('2019', '10', '02', '19', '25', '10'),
+                'absent_status' => 0,
+                'absent_reason' => null,
+                'revision_status' => 1,
+                'revision_request' => '電車が遅延していたため遅刻しました。',
+            ],
+            [
+                'user_id' => 3,
+                'date_time' => Carbon::create('2019', '09', '22'),
+                'start_time' => Carbon::create('2019', '09', '22', '09', '15', '20'),
+                'end_time' => null,
+                'absent_status' => 0,
+                'absent_reason' => null,
+                'revision_status' => 0,
+                'revision_request' => null,
+            ],
+            [
+                'user_id' => 1,
+                'date_time' => Carbon::create('2019', '10', '05'),
+                'start_time' => Carbon::create('2019', '10', '05', '09', '35', '50'),
+                'end_time' => null,
+                'absent_status' => 0,
+                'absent_reason' => null,
+                'revision_status' => 1,
+                'revision_request' => '用事のため途中で帰りました',
+            ],
+            [
+                'user_id' => 4,
+                'date_time' => Carbon::create('2019', '10', '02'),
+                'start_time' => null,
+                'end_time' => null,
+                'absent_status' => 1,
+                'absent_reason' => '体調不良のため',
+                'revision_status' => 0,
+                'revision_request' => null,
+            ],
+            [
+                'user_id' => 2,
+                'date_time' => Carbon::create('2019', '10', '02'),
+                'start_time' => null,
+                'end_time' => null,
+                'absent_status' => 1,
+                'absent_reason' => '体調不良のため',
+                'revision_status' => 1,
+                'revision_request' => '病院で処方された薬を飲んだら嘘のように元気になったので午後から出社しています。',
+            ],
+            [
+                'user_id' => 4,
+                'date_time' => Carbon::create('2019', '10', '15'),
+                'start_time' => Carbon::create('2019', '10', '15', '09', '45', '10'),
+                'end_time' => Carbon::create('2019', '10', '15', '21', '15', '10'),
+                'absent_status' => 0,
+                'absent_reason' => null,
+                'revision_status' => 1,
+                'revision_request' => '課題締め切り日が近いため残業しました',
+            ],
+        ]);
+    }
+}
