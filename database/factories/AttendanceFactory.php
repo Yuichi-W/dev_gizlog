29bd6567732aa8e974041efeded58e46e612aefc
YUICHIRO WADA <wada1257@gmail.com>
2019-11-05 13:45:17 +0900

    factoryでダミーデータを追加

---
+++ database/factories/AttendanceFactory.php
@@ -0,0 +1,46 @@
+<?php
+
+use Faker\Generator as Faker;
+use App\Models\Attendance;
+use Carbon\Carbon;
+
+$factory->define(Attendance::class, function (Faker $faker) {
+    $dateTime = new Carbon($faker->dateTime()->format('Y-m-d H:i:s'));
+    $hourAm = $faker->numberBetween(7, 11);
+    $hourPm = $faker->numberBetween(18, 23);
+    $minuteAm = $faker->numberBetween(0, 59);
+    $minutePm = $faker->numberBetween(0, 59);
+    $secondAm = $faker->numberBetween(0, 59);
+    $secondPm = $faker->numberBetween(0, 59);
+    $startTime = $dateTime->setTime($hourAm, $minuteAm, $secondAm)->toDateTimeString();
+    $endTime = $dateTime->setTime($hourPm, $minutePm, $secondPm)->toDateTimeString();
+
+    return [
+        'user_id'    => $faker->numberBetween(1, 4),
+        'date'       => $dateTime,
+        'start_time' => $startTime,
+        'end_time'   => $endTime,
+    ];
+});
+
+$factory->state(Attendance::class, 'traning', function(Faker $faker) {
+    return [
+    'end_time' => null,
+    ];
+});
+
+$factory->state(Attendance::class, 'absent', function(Faker $faker) {
+    return [
+    'start_time'    => null,
+    'end_time'      => null,
+    'absent_status' => 1,
+    'absent_reason' => $faker->randomElement(['体調不良のため', '病院に行くため', '事故にあったため']),
+    ];
+});
+
+$factory->state(Attendance::class, 'modify', function(Faker $faker) {
+    return [
+    'revision_status' => 1,
+    'revision_request' => $faker->randomElement(['出勤登録を間違えました、10時出訂正おねがします。', '電車遅延したため遅れました', '道端のお婆さん助けてました']),
+    ];
+});
--- database/seeds/AttendancesTableSeeder.php
+++ database/seeds/AttendancesTableSeeder.php
@@ -1,110 +1,26 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Models\Attendance;
 use Carbon\Carbon;
 
 class AttendancesTableSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * 関数概要　 : 作成レコードは下記の４種
+     * 「出社」 　: start_time,end_time共に同一日時で値（時間）があるstart_timeの方が早い
+     * 「研修中」 : start_timeに値があり、end_timeがnullである
+     * 「欠席」 　: start_time,end_time共にnullでabsent_statusが1,absent_resonに文字列が入る
+     * 「申請」 　: start_time,end_time共に同一日時で値（時間）があり、revision_statusが1,revision_requestに文字列が入る
      *
      * @return void
      */
     public function run()
     {
         DB::table('attendances')->truncate();
-        DB::table('attendances')->insert([
-
-            [
-                'user_id' => 1,
-                'date' => Carbon::create('2019', '10', '01'),
-                'start_time' => Carbon::create('2019', '10', '01', '09', '45', '00'),
-                'end_time' => Carbon::create('2019', '10', '01', '19', '05', '00'),
-                'absent_status' => 0,
-                'absent_reason' => null,
-                'revision_status' => 0,
-                'revision_request' => null,
-            ],
-            [
-                'user_id' => 2,
-                'date' => Carbon::create('2019', '10', '02'),
-                'start_time' => Carbon::create('2019', '10', '02', '10', '15', '20'),
-                'end_time' => Carbon::create('2019', '10', '02', '19', '25', '10'),
-                'absent_status' => 0,
-                'absent_reason' => null,
-                'revision_status' => 1,
-                'revision_request' => '電車が遅延していたため遅刻しました。',
-            ],
-            [
-                'user_id' => 3,
-                'date' => Carbon::create('2019', '09', '22'),
-                'start_time' => Carbon::create('2019', '09', '22', '09', '15', '20'),
-                'end_time' => null,
-                'absent_status' => 0,
-                'absent_reason' => null,
-                'revision_status' => 0,
-                'revision_request' => null,
-            ],
-            [
-                'user_id' => 1,
-                'date' => Carbon::create('2019', '10', '05'),
-                'start_time' => Carbon::create('2019', '10', '05', '09', '35', '50'),
-                'end_time' => null,
-                'absent_status' => 0,
-                'absent_reason' => null,
-                'revision_status' => 1,
-                'revision_request' => '用事のため途中で帰りました',
-            ],
-            [
-                'user_id' => 4,
-                'date' => Carbon::create('2019', '10', '02'),
-                'start_time' => null,
-                'end_time' => null,
-                'absent_status' => 1,
-                'absent_reason' => '体調不良のため',
-                'revision_status' => 0,
-                'revision_request' => null,
-            ],
-            [
-                'user_id' => 2,
-                'date' => Carbon::create('2019', '10', '02'),
-                'start_time' => null,
-                'end_time' => null,
-                'absent_status' => 1,
-                'absent_reason' => '体調不良のため',
-                'revision_status' => 1,
-                'revision_request' => '病院で処方された薬を飲んだら嘘のように元気になったので午後から出社しています。',
-            ],
-            [
-                'user_id' => 4,
-                'date' => Carbon::create('2019', '10', '15'),
-                'start_time' => Carbon::create('2019', '10', '15', '09', '45', '10'),
-                'end_time' => Carbon::create('2019', '10', '15', '21', '15', '10'),
-                'absent_status' => 0,
-                'absent_reason' => null,
-                'revision_status' => 1,
-                'revision_request' => '課題締め切り日が近いため残業しました',
-            ],
-            [
-                'user_id' => 4,
-                'date' => Carbon::create('2019', '10', '25'),
-                'start_time' => Carbon::create('2019', '10', '25', '09', '25', '10'),
-                'end_time' => Carbon::create('2019', '10', '25', '19', '15', '10'),
-                'absent_status' => 0,
-                'absent_reason' => null,
-                'revision_status' => 1,
-                'revision_request' => '早く来すぎました',
-            ],
-            [
-                'user_id' => 4,
-                'date' => Carbon::create('2019', '10', '27'),
-                'start_time' => Carbon::create('2019', '10', '27', '10', '00', '10'),
-                'end_time' => Carbon::create('2019', '10', '27', '19', '35', '10'),
-                'absent_status' => 0,
-                'absent_reason' => null,
-                'revision_status' => 1,
-                'revision_request' => '10秒の遅刻',
-            ],
-        ]);
+        factory(Attendance::class, 100)->create();
+        factory(Attendance::class, 100)->states('traning')->create();
+        factory(Attendance::class, 100)->states('absent')->create();
+        factory(Attendance::class, 100)->states('modify')->create();
     }
 }
